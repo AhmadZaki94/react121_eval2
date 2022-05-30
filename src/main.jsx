e305f3a17d1a978ed00838984cae89e66aa17862
Ahmad Zaki <shaikhzaki8@gmail.com>
2022-05-30 17:21:40 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,14 @@ import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-      <AuthContextProvider>
         <App />
-      </AuthContextProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
